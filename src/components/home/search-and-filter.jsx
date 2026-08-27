4793891db3fef71793e6b5066aba3e0fdde7fb74
Naeem <naimur1439n@gmail.com>
2026-08-27 14:27:04 +0600

    home page fixed is smaller screens

--- src/components/home/search-and-filter.jsx
+++ src/components/home/search-and-filter.jsx
@@ -3,7 +3,7 @@ import TagFilter from "./tag-filter";
 
 export default function SearchAndFilter({ recipes, setFilteredRecipes }) {
   return (
-    <section className="flex h-18 flex-col items-center justify-between gap-4 p-4 sm:flex-row">
+    <section className="flex flex-col items-center justify-between gap-3 p-4 sm:h-18 sm:flex-row sm:gap-4">
       <SearchBar recipes={recipes} setFilteredRecipes={setFilteredRecipes} />
 
       <TagFilter recipes={recipes} setFilteredRecipes={setFilteredRecipes} />
