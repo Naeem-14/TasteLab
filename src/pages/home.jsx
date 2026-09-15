b69f43da531c53329d1deb0b62a43e0eb821d198
Naeem <naimur1439n@gmail.com>
2026-09-15 18:48:46 +0600

    test fix

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -14,7 +14,7 @@ export default function Home({ recipes, setRecipes, isLoggedIn, user }) {
   return (
     <div className="grid grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
       <Header user={user} isLoggedIn={isLoggedIn} />
-      {/* <Sidebar user={user} isLoggedIn={isLoggedIn} /> */}
+      <Sidebar user={user} isLoggedIn={isLoggedIn} />
       <main className="col-span-3 flex h-[calc(100vh-48px)] flex-col bg-bg sm:col-span-2 lg:col-span-3 xl:col-span-4">
         <SearchAndFilter setRecipesToFilter={setRecipes} data={data} />
         <section className="flex-1 scrollbar-none overflow-y-auto px-4 pt-2">
