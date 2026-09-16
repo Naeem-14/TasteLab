9b1fb5a1d111b46ae9b0d4d4345a6c72aa05ccbc
Naeem <naimur1439n@gmail.com>
2026-09-16 14:10:41 +0600

    fix image not showing in host server

--- src/features/cooking101/cooking-101-data.jsx
+++ src/features/cooking101/cooking-101-data.jsx
@@ -11,7 +11,7 @@ const cooking101Data = [
         title: "Temperature",
         description:
           "Convert effortlessly temperatures for seamless oven prep.",
-        image: "../../assets/temperature.jpg",
+        image: "/assets/temperature.jpg",
         path: "/temperature-converter",
       },
 
@@ -19,13 +19,13 @@ const cooking101Data = [
         title: "Volume",
         description:
           "Quickly convert liquid and dry measures from milliliters to cups and spoons.",
-        image: "../../assets/volume.jpg",
+        image: "/assets/volume.jpg",
         path: "/volume-converter",
       },
       {
         title: "Weight",
         description: "Translate weights for accurate baking and portioning.",
-        image: "../../assets/weight.jpg",
+        image: "/assets/weight.jpg",
         path: "/weight-converter",
       },
     ],
@@ -40,21 +40,21 @@ const cooking101Data = [
         title: "Knife Skills & Cuts",
         description:
           "Master foundational cutting techniques, hand positioning, and uniform prep size.",
-        image: "../../assets/knife-skills.jpg",
+        image: "/assets/knife-skills.jpg",
         path: "/knife-skills-and-cuts",
       },
       {
         title: "Food Safety & Prep",
         description:
           "Essential safe internal cooking temperatures and hygienic food thawing methods.",
-        image: "../../assets/food-safety.jpg",
+        image: "/assets/food-safety.jpg",
         path: "/food-safety-and-prep",
       },
       {
         title: "Flavor Balancing",
         description:
           "Troubleshoot seasonings by balancing acid, salt, fat, and heat in any dish.",
-        image: "../../assets/flavor-balancing.jpg",
+        image: "/assets/flavor-balancing.jpg",
         path: "/flavor-balancing",
       },
     ],
@@ -69,21 +69,21 @@ const cooking101Data = [
         title: "Smoke Points",
         description:
           "Match cooking oils to proper heat thresholds to prevent burnt flavors.",
-        image: "../../assets/smoke-points.jpg",
+        image: "/assets/smoke-points.jpg",
         path: "/smoke-points",
       },
       {
         title: "Pantry Staples Guide",
         description:
           "Must-have long-lasting ingredients to build quick, versatile meals anytime.",
-        image: "../../assets/pantry-staple.jpg",
+        image: "/assets/pantry-staple.jpg",
         path: "/pantry-staples-guide",
       },
       {
         title: "Basic Sauces",
         description:
           "Learn some basic sauces and simple ratios to elevate any plate.",
-        image: "../../assets/basic-sauces.jpg",
+        image: "/assets/basic-sauces.jpg",
         path: "/basic-sauces",
       },
     ],
--- src/pages/Cooking101.jsx
+++ src/pages/Cooking101.jsx
@@ -1,16 +1,6 @@
 import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import RenderPages from "../features/cooking101/RenderPages";
-import { Routes, Route } from "react-router";
-import TemperatureConvertor from "../features/cooking101/pages/temperature-converter/TemeperatureConverter";
-import VolumeConverter from "../features/cooking101/pages/volume-converter/VolumeConverter";
-import WeightConverter from "../features/cooking101/pages/weight-converter/WeightConverter";
-import KnifeSkills from "../features/cooking101/pages/knife-skills/KnifeSkills";
-import FoodSafetyAndPrep from "../features/cooking101/pages/food-safety/FoodSafety";
-import FlavorBalancing from "../features/cooking101/pages/flavor-balancing/FlavorBalancing";
-import SmokePoints from "../features/cooking101/pages/smoke-point/SmokePoint";
-import PantryStaples from "../features/cooking101/pages/pantry-staples/PantryStaple";
-import BasicSauces from "../features/cooking101/pages/basic-saues/BasicSauces";
 
 export default function Cooking101({ user, isLoggedIn }) {
   return (
@@ -18,9 +8,7 @@ export default function Cooking101({ user, isLoggedIn }) {
       <Header user={user} isLoggedIn={isLoggedIn} />
       <Sidebar user={user} isLoggedIn={isLoggedIn} />
       <main className="col-span-3 grid h-[calc(100vh-48px)] scrollbar-none gap-4 overflow-y-scroll bg-bg p-5 sm:col-span-2 lg:col-span-3 xl:col-span-4">
-        <Routes>
-          <Route index element={<RenderPages />} />
-        </Routes>
+        <RenderPages />
       </main>
     </div>
   );
