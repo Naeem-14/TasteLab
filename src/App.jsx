4fa061355fe1381154ee46ca064a842f44836618
Naeem <naimur1439n@gmail.com>
2026-09-16 12:33:30 +0600

    Home rename fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 import data from "./data/data";
-import Home from "./pages/HomeFix.jsx";
+import Home from "./pages/Home.jsx";
 import AddRecipe from "./pages/AddRecipe";
 import OwnRecipes from "./pages/OwnRecipes";
 import Cooking101 from "./pages/Cooking101";
